32636ee75c4dc63645c1b69e54498cbbfde621e1
AQ <alexquayle10@gmail.com>
2024-01-19 00:37:20 +0000

    Revised time block loop to add the correct hours instead of a placeholder.

--- script.js
+++ script.js
@@ -5,19 +5,18 @@ let dateFormat = dayjs().format('dddd, MMMM D');
 date.append(dateFormat);
 // Accesses the container where the timeblocks go
 let timeBlock = $('.container');
-// Stores the time block content
-let blockContent =
-    `<div class="container">
-    <div class="row">
-      <div class="col-2 hour d-flex justify-content-center align-items-center">Hour</div>
-      <div class="col-8 future d-flex align-items-center">Description</div>
-      <div class="col-2 saveBtn d-flex justify-content-center align-items-center">
-        <span class="material-symbols-outlined">save</span>
-      </div>
-    </div>
-  </div>`;
-
+let hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 // Adds a time block to the page 8 times
-for (let i = 0; i < 8; i++) {
+for (let i = 0; i < 9; i++) {
+    // Stores the time block content
+    let blockContent = `<div class="container">
+    <div class="row">
+        <div class="col-2 hour d-flex justify-content-center align-items-center">${hours[i]}</div>
+        <div class="col-8 future d-flex align-items-center">New event</div>
+        <div class="col-2 saveBtn d-flex justify-content-center align-items-center">
+    <span class="material-symbols-outlined">save</span>
+    </div>
+    </div>
+</div>`;
     timeBlock.append(blockContent);
 }
