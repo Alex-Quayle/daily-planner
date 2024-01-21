67fa792cce35e5c2fe2aa0dac9737839d21a2367
AQ <alexquayle10@gmail.com>
2024-01-21 17:27:31 +0000

    Feature: Added if/else block to assess the time of day and update the text area colours accordingly.

--- script.js
+++ script.js
@@ -5,28 +5,24 @@ let dateFormat = dayjs().format('dddd, MMMM D');
 date.append(dateFormat);
 // Accesses the container where the timeblocks go
 let timeBlock = $('.container');
-let hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
+let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 // Adds a time block to the page 8 times
 for (let i = 0; i < 9; i++) {
-    // Stores the time block content
-    let blockContent = `<div class="container">
+    let blockContent = $(`<div class="container">
     <div class="row">
         <div class="col-2 hour d-flex justify-content-center align-items-center">${hours[i]}</div>
-        <textarea type="text" class="col-8 future d-flex align-items-center user-input"></textarea>
+        <textarea type="text" class="future col-8 d-flex align-items-center user-input"></textarea>
         <button class="col-2 saveBtn d-flex justify-content-center align-items-center">
     <span class="material-symbols-outlined">save</span>
     </button>
     </div>
-</div>`;
+</div>`);
+// Changes the textarea colour based on the time of day, .future is the default thus not needed in this if/else block
+    if (hours[i] < dayjs().hour()) {
+        blockContent.find('textarea').addClass('past');
+    } else {
+        blockContent.find('textarea').addClass('present')
+    }
     timeBlock.append(blockContent);
 }
-
-
-{/* <div class="row">
-<div class="col-2 hour d-flex justify-content-center align-items-center">${hours[i]}</div>
-<div class="col-8 future d-flex align-items-center"><textarea type="text" class="user-input" placeholder="Enter event"></textarea></div>
-<button class="col-2 saveBtn d-flex justify-content-center align-items-center">
-<span class="material-symbols-outlined">save</span>
-</button>
-</div>
-</div>`; */}
+console.log(dayjs().hour());
